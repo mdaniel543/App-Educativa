2b6443e96b9113e9c9b4865965786799f8fb0545
szapeta <2375894860101@ingenieria.usac.edu.gt>
2022-04-19 11:05:00 -0600

    Create Alumnos_sp.sql
    Creacion de ABC Alumnos

---
+++ bdd/Alumnos_sp.sql
@@ -0,0 +1,101 @@
+-- Creacion de usuario Estudiante
+DELIMITER //
+Drop PROCEDURE if EXISTS alumno_create//
+CREATE PROCEDURE alumno_create(par_nombre VARCHAR(100), par_apellido VARCHAR(100), par_carnet VARCHAR(100),  par_telefono VARCHAR(100), par_direccion VARCHAR(100), par_correo VARCHAR(100), par_pass VARCHAR(100))
+BEGIN
+
+	DECLARE resp VARCHAR(100) DEFAULT '';
+	DECLARE msg_err VARCHAR(100) DEFAULT '';
+
+	DECLARE numresp int;
+
+	select COUNT(*) into numresp from Alumno where carnet = par_carnet;
+
+	if (numresp >0) THEN
+		set msg_err = 'Alumno ya existe';
+	ELSE
+		insert into Alumno(Nombre, Apellido, Carnet, Telefono, Direccion, Correo_electronico, Pass, Estado) values (par_nombre, par_apellido, par_carnet, par_telefono, par_direccion, par_correo, par_pass, 1);
+		SELECT LAST_INSERT_ID() into resp;
+	END IF;
+
+	SELECT msg_err, resp;
+
+END//
+DELIMITER ;
+
+-- Alumno actualizacion y eliminacion
+DELIMITER //
+Drop PROCEDURE if EXISTS alumno_update_delete//
+CREATE PROCEDURE alumno_update_delete(par_tipo_operacion int, 
+par_nombre VARCHAR(100), par_apellido VARCHAR(100), 
+par_carnet VARCHAR(100),  par_telefono VARCHAR(100), 
+par_direccion VARCHAR(100), par_correo VARCHAR(100), 
+par_estado int)
+BEGIN
+
+	DECLARE resp VARCHAR(100) DEFAULT '';
+	DECLARE msg_err VARCHAR(100) DEFAULT '';
+    DECLARE numresp int;
+
+    select COUNT(*) into numresp from Alumno where carnet = par_carnet;
+
+    if( numresp = 1) then
+      if (par_tipo_operacion = 1) THEN
+            update Alumno
+            set Nombre = par_nombre,
+            Apellido = par_apellido, Telefono = par_telefono, Direccion = par_direccion, Correo_electronico = par_correo
+            where carnet = par_carnet;
+            
+            set resp = 'datos actualizados correctamente';
+        elseif (par_tipo_operacion = 2) THEN
+            update Alumno
+            set estado = par_estado
+            where carnet = par_carnet;
+
+            set resp = 'alumno cambio de estado';
+        else
+            set msg_err = 'operacion no reconocida';
+        end if;
+    else
+      set msg_err = 'alumno no encontrado';
+    end if;
+
+	SELECT msg_err, resp;
+
+END//
+DELIMITER ;
+
+
+-- Alumno obtiene todos los registros de la tabla alumno
+DELIMITER //
+Drop PROCEDURE if EXISTS alumno_TODO//
+CREATE PROCEDURE alumno_TODO()
+BEGIN
+
+	DECLARE resp VARCHAR(100) DEFAULT '';
+	DECLARE msg_err VARCHAR(100) DEFAULT '';
+    DECLARE numresp int;
+
+    select * from Alumno;
+
+END//
+DELIMITER ;
+
+-- Alumno se obtiene uno en especifico
+DELIMITER //
+Drop PROCEDURE if EXISTS alumno_get_by_id//
+CREATE PROCEDURE alumno_get_by_id(par_idalumno int, par_carnet varchar(100))
+BEGIN
+
+	DECLARE resp VARCHAR(100) DEFAULT '';
+	DECLARE msg_err VARCHAR(100) DEFAULT '';
+    DECLARE numresp int;
+
+    if(par_carnet != '') then
+        select * from Alumno where carnet = par_carnet;
+    else
+        select * from Alumno where idAlumno = par_idalumno;
+    end if;
+    
+END//
+DELIMITER ;
