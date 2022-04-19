cdaaf03bad21ba35364b4dc77a5a4bcc62059093
szapeta <2375894860101@ingenieria.usac.edu.gt>
2022-04-19 09:03:11 -0600

    Update SP.sql
    Se crea el SP de creacion de alumnos

--- bdd/SP.sql
+++ bdd/SP.sql
@@ -49,3 +49,30 @@ BEGIN
 
 END //
 DELIMITER ;
+
+
+-- Creacion de usuario Estudiante
+DELIMITER //
+Drop PROCEDURE if EXISTS alumno_create//
+CREATE PROCEDURE alumno_create(par_nombre VARCHAR(100), par_apellido VARCHAR(100), par_carnet VARCHAR(100),  par_telefono VARCHAR(100), par_direccion VARCHAR(100), par_correo VARCHAR(100), par_pass VARCHAR(100))
+BEGIN
+
+	DECLARE resp VARCHAR(100) DEFAULT '';
+	DECLARE msg_err VARCHAR(100) DEFAULT '';
+
+	DECLARE numresp int;
+
+	select COUNT(*) into numresp from Alumno where carnet = par_carnet;
+
+	if (numresp >0) THEN
+		set msg_err = 'Alumno ya existe';
+	ELSE
+		insert into Alumno(Nombre, Apellido, Carnet, Telefono, Direccion, Correo_electronico, Pass) values (par_nombre, par_apellido, par_carnet, par_telefono, par_direccion, par_correo, par_pass);
+		SELECT LAST_INSERT_ID() into resp;
+	END IF;
+
+	SELECT msg_err, resp;
+
+END//
+DELIMITER ;
+
