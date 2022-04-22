978177dd9e61a39c35e5ed8e08cd539ab9b38d80
Christtopher <ccristoferjose@gmail.com>
2022-04-21 21:52:27 -0700

    assign curso carrera

---
+++ node_js/services/assing.js
@@ -0,0 +1,35 @@
+const db = require("./db");
+const helper = require("../helper");
+const config = require("../config");
+
+async function assign_alumno_curso(req,res){
+    let data = req.body;
+    const rows = await db.query(
+        ``
+    );
+    res.json({});
+}
+
+
+async function assing_curso_carrera(req,res){
+    let data = req.body;
+    const rows = await db.query(
+        `CALL pensum_asignar(${data.idCarrera},${data.idCurso});`
+    );
+
+    const resp = rows[0];
+  if (resp[0].msg_err != "") {
+    console.log(resp[0].msg_err);
+    return {
+      msg: resp[0].msg_err,
+    };
+  }
+  res.json({ msg: resp[0].resp});
+
+}
+
+
+module.exports = {
+    assign_alumno_curso,
+    assing_curso_carrera,
+}
