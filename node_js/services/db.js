de359fd79f63f95c452af307bf388b8f037d960a
Christtopher <ccristoferjose@gmail.com>
2022-04-24 17:39:34 -0700

    fixing connection

--- node_js/services/db.js
+++ node_js/services/db.js
@@ -5,6 +5,16 @@ const config = require('../config');
 async function query(sql,params){
     const connection = await mysql.createConnection(config.db);
     const [results, ] = await connection.execute(sql,params);
+
+    connection.end(function(err){
+        console.log('clossing connection');
+        if(err){
+            return console.log('error: ' + err.message);
+        }
+        console.log('Close the dtabase connection');
+    });
+    connection.destroy();
+
     return results;
 }
 
